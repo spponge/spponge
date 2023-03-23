3050e97315bbe4d94c1a16aa775dd01a82372e74
chamchimayo <chamchi.maayo@gmail.com>
2023-03-23 17:51:39 +0900

    [add]회원가입: controller에 예외처리문 추가

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,11 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Res, Get, Post, Body, Patch, Param, Delete,  } from '@nestjs/common';
+import { Controller, Res, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/input/create-user.dto';
 import { LoginUserDto } from './dto/input/login-user.dto';
-import { CreateOutputDto } from './dto/output/create';
-import { LoginOutputDto } from './dto/output/login.dto';
 
 @Controller('user')
 export class UserController {
@@ -14,17 +12,21 @@ export class UserController {
     @ApiTags('User')
     @Post()
     async create(@Body() createUserDto: CreateUserDto): Promise<void> {
-        await this.userService.create(createUserDto);
-        return;
+        try {
+            await this.userService.create(createUserDto);
+            return;
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @Post('login')
     async login(@Body() loginUserDto: LoginUserDto, @Res({ passthrough: true }) res: Response): Promise<void> {
         const jwt = await this.userService.login(loginUserDto);
-        res.cookie('jwt', jwt.access_token,{
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000, //1 day
-        });
-        return ;
+        // res.cookie('jwt', jwt.access_token,{
+        //     httpOnly: true,
+        //     maxAge: 24 * 60 * 60 * 1000, //1 day
+        // });
+        return;
     }
 }
