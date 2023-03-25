ddf35a123eb7f9ed7baeab2129772ad54de8ec61
김민섭 <kimminseob@gimminseob-ui-MacBookPro.local>
2023-03-25 13:12:25 +0900

    [fix] test3

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'actions Test2!';
+        return 'actions Test3!';
     }
 }
