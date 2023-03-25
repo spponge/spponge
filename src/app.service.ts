b97384693757b9c695678aaac720fad7d96cb29a
김민섭 <kimminseob@gimminseob-ui-MacBookPro.local>
2023-03-25 12:54:53 +0900

    [fix] SSH_KEY 수정

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'webHook Test!';
-  }
+    getHello(): string {
+        return 'actions Test!';
+    }
 }
