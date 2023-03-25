3622bed577322e4093ec375a82682b7ca2de8600
김민섭 <kimminseob@gimminseob-ui-MacBookPro.local>
2023-03-25 13:19:21 +0900

    [fix] 방화벽 허용 및 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.use(cookieParser());
-  const config = new DocumentBuilder()
-  .setTitle('AR Makers API')
-  .setDescription('AR Makers API Document')
-  .setVersion('1.0')
-  .build();
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.use(cookieParser());
+    const config = new DocumentBuilder()
+        .setTitle('AR Makers API')
+        .setDescription('AR Makers API Document')
+        .setVersion('1.0')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-    await app.listen(3000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    await app.listen(5000);
 }
 bootstrap();
