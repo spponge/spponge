e761f90a87d41221ccf010d3966ae3a73ed003e4
chamchimayo <chamchi.maayo@gmail.com>
2023-03-29 16:25:15 +0900

    [add]이메일 인증: mailer module 작성, mailer service 생성

---
+++ src/domain/user/mailer/mailer.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailerService } from './mailer.service';
+
+@Module({
+    imports: [
+        MailerModule.forRoot({
+            transport: {
+                host: process.env.EMAIL_HOST,
+                port: process.env.EMAIL_PORT,
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            },
+            defaults: {
+                from: '"nest-modules" <modules@nestjs.com>',
+            },
+            template: {
+                dir: __dirname + '/templates',
+                adapter: new HandlebarsAdapter(),
+                options: {
+                    strict: true,
+                },
+            },
+        }),
+    ],
+    providers: [MailService],
+    exports: [MailService],
+})
+export class MailModule {}
