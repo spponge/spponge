81cf4e13fde5d955f6f30dff48091eba8450b34f
chamchimayo <chamchi.maayo@gmail.com>
2023-04-03 20:53:18 +0900

    [add]댓글 수정: controller에 update(댓글 수정) 작성

--- src/domain/comment/comment.controller.ts
+++ src/domain/comment/comment.controller.ts
@@ -1,23 +1,37 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post, Patch, Req, UseGuards, Param } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/common/auth/jwt-auth.guard';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/input/create-comment.dto';
 import { JwtPayload } from 'src/common/auth/interfaces/jwt-payload.interface';
+import { UpdateCommentDto } from './dto/input/update-comment.dto';
 
 @ApiTags('Comment')
 @Controller('comment')
 export class CommentController {
     constructor(private readonly commentService: CommentService) {}
 
-    @ApiOperation({ summary: '댓글작성 API' })
+    @ApiOperation({ summary: '댓글 작성 API' })
     @UseGuards(JwtAuthGuard)
     @Post()
     async create(
         @Body() createCommentDto: CreateCommentDto,
         @Req() req: Request & { user: JwtPayload },
     ): Promise<void> {
+        console.log(`@Controller: ${req.user.id}`);
         return await this.commentService.create(createCommentDto, req.user.id);
+    }
+
+    @ApiOperation({ summary: '댓글 수정 API' })
+    // @UseGuards(JwtAuthGuard)
+    @Patch(':id')
+    async update(
+        @Param('id') id: number,
+        // @Req() req: Request & { user: JwtPayload },
+        @Body() updateCommentDto: UpdateCommentDto,
+    ): Promise<void> {
+        return await this.commentService.update(id, updateCommentDto);
     }
 }
