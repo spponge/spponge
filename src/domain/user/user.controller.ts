5e6ce6d03095345ca7422ce4ba3c21f351d9b9bc
chamchimayo <chamchi.maayo@gmail.com>
2023-04-23 22:35:08 +0900

    [add]회원 조회: controller에 findOne(로그인 유저 정보 조회) 작성

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/input/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { User } from 'src/common/decorator/user.decorator';
+import { Users } from 'src/entity/user.entities';
 
 @ApiTags('User')
 @Controller('user')
@@ -14,5 +17,12 @@ export class UserController {
     async create(@Body() createUserDto: CreateUserDto): Promise<void> {
         await this.userService.create(createUserDto);
         return;
+    }
+
+    @ApiOperation({ summary: '회원 정보 조회 API' })
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async findOne(@User() user): Promise<Users> {
+        return await this.userService.findOne(user.id);
     }
 }
